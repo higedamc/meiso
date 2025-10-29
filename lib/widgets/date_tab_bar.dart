7122f023dd9b3b794417e41b79bb5b0475a15691
higedamc <higedamc@example.com>
2025-10-29 19:28:03 +0900

    update: date tab bar

--- lib/widgets/date_tab_bar.dart
+++ lib/widgets/date_tab_bar.dart
@@ -3,8 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import '../app_theme.dart';
 
-/// 日付タブバー
-class DateTabBar extends StatelessWidget {
+/// 日付タブバー（横スクロール対応）
+class DateTabBar extends ConsumerStatefulWidget {
   const DateTabBar({
     required this.dates,
     required this.currentIndex,
@@ -17,10 +17,61 @@ class DateTabBar extends StatelessWidget {
   final Function(int) onDateTap;
 
   @override
-  Widget build(BuildContext context) {
-    // 現在の日付を中心に5日分表示
-    final displayDates = _getDisplayDates();
+  ConsumerState<DateTabBar> createState() => _DateTabBarState();
+}
 
+class _DateTabBarState extends ConsumerState<DateTabBar> {
+  late ScrollController _scrollController;
+  static const double _itemWidth = 80.0; // 各日付タブの幅
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    // 初期位置を設定
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToIndex(widget.currentIndex, animate: false);
+    });
+  }
+
+  @override
+  void didUpdateWidget(DateTabBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // 選択されたインデックスが変わったら自動スクロール
+    if (oldWidget.currentIndex != widget.currentIndex) {
+      _scrollToIndex(widget.currentIndex);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  /// 指定されたインデックスにスクロール
+  void _scrollToIndex(int index, {bool animate = true}) {
+    if (!_scrollController.hasClients) return;
+
+    // 画面の中央に表示されるようにスクロール位置を計算
+    final screenWidth = MediaQuery.of(context).size.width;
+    final targetPosition = (index * _itemWidth) - (screenWidth / 2) + (_itemWidth / 2);
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final scrollTo = targetPosition.clamp(0.0, maxScroll);
+
+    if (animate) {
+      _scrollController.animateTo(
+        scrollTo,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _scrollController.jumpTo(scrollTo);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       height: 90,
       decoration: BoxDecoration(
@@ -33,68 +84,57 @@ class DateTabBar extends StatelessWidget {
           ],
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: displayDates.map((dateInfo) {
-          final isSelected = dateInfo['index'] == currentIndex;
-          return Expanded(
-            child: InkWell(
-              onTap: () => onDateTap(dateInfo['index'] as int),
-              child: Container(
-                decoration: isSelected
-                    ? BoxDecoration(
-                        color: Colors.white.withOpacity(0.3),
-                        shape: BoxShape.circle,
-                      )
-                    : null,
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      dateInfo['monthDay'] as String,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: isSelected ? 16 : 14,
-                        fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        physics: const ClampingScrollPhysics(),
+        child: Row(
+          children: List.generate(widget.dates.length, (index) {
+            final date = widget.dates[index];
+            final isSelected = index == widget.currentIndex;
+            
+            return SizedBox(
+              width: _itemWidth,
+              child: InkWell(
+                onTap: () => widget.onDateTap(index),
+                child: Container(
+                  decoration: isSelected
+                      ? BoxDecoration(
+                          color: Colors.white.withOpacity(0.3),
+                          shape: BoxShape.circle,
+                        )
+                      : null,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        DateFormat('M/d').format(date),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: isSelected ? 16 : 14,
+                          fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      dateInfo['weekday'] as String,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: isSelected ? 20 : 16,
-                        fontWeight: FontWeight.w700,
-                        letterSpacing: 0.5,
+                      const SizedBox(height: 4),
+                      Text(
+                        DateFormat('EEE', 'en_US').format(date).toUpperCase(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: isSelected ? 20 : 16,
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: 0.5,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        }).toList(),
+            );
+          }),
+        ),
       ),
     );
-  }
-
-  /// 表示する日付を取得（現在を中心に前後2日）
-  List<Map<String, dynamic>> _getDisplayDates() {
-    final result = <Map<String, dynamic>>[];
-    final start = (currentIndex - 2).clamp(0, dates.length - 5);
-    final end = (start + 5).clamp(5, dates.length);
-
-    for (var i = start; i < end; i++) {
-      final date = dates[i];
-      result.add({
-        'index': i,
-        'monthDay': DateFormat('M/d').format(date),
-        'weekday': DateFormat('EEE', 'en_US').format(date).toUpperCase(),
-      });
-    }
-
-    return result;
   }
 }
 
